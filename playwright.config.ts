a842533163c0b2e4e924f8a2e451f750a54ae3ff
David Sima <davids@ensemble.com>
2024-04-05 15:50:34 +0300

    test: added serve to the dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx serve . --cors',
+    command: 'pnpm serve . --cors',
     url: 'http://localhost:3000/',
     reuseExistingServer: !process.env.CI,
   },
