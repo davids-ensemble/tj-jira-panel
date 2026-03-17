401b5f2050a6808a8142a9ae694ace96facf0385
David Sima <davids@ensemble.com>
2026-03-17 23:08:26 +0200

    feat: scaffold tj-unsubmitted-banner component

---
+++ src/components/tj-jira-panel/components/unsubmitted-banner/tj-unsubmitted-banner.tsx
@@ -0,0 +1,12 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'tj-unsubmitted-banner',
+  styleUrl: 'tj-unsubmitted-banner.css',
+  scoped: true,
+})
+export class TJUnsubmittedBanner {
+  render() {
+    return <div></div>;
+  }
+}
